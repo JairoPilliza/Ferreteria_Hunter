b87eb5ada0e5057a733a97a521731ecede2d7123
JairoPilliza <84792503+JairoPilliza@users.noreply.github.com>
2021-11-05 16:48:52 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new mysqli("localhost","root","","ferreteria");
+	$db = new mysqli("alfamysql.mysql.database.azure.com","administrador","dev12345#","ferreteria");
 	if (mysqli_connect_errno()) {
 		echo "No se puede conectar 🚫";
 	}
