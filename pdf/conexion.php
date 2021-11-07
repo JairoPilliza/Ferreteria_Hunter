0d9d605e0da90f66ce681e2c336c7c12b5915c78
JairoPilliza <84792503+JairoPilliza@users.noreply.github.com>
2021-11-06 20:20:24 -0500

    Update conexion.php

--- pdf/conexion.php
+++ pdf/conexion.php
@@ -1,10 +1,8 @@
 <?php
-	
-	$mysqli = new mysqli('localhost', 'root', '', 'ferreteria');
-	
-	if($mysqli->connect_error){
-		
-		die('Error en la conexion' . $mysqli->connect_error);
-		
+$db = new mysqli("alfamysql.mysql.database.azure.com:3306","administrador","dev12345#","ferreteria");
+	if (mysqli_connect_errno()) {
+		echo "No se puede conectar 🚫";
+
 	}
+	/*$db = mysqli_init(); mysqli_ssl_set($db,NULL,NULL, "", NULL, NULL); mysqli_real_connect($db, "alfamysql.mysql.database.azure.com", "administrador", "dev12345#", "sys", 3306, MYSQLI_CLIENT_SSL);*/
 ?>
